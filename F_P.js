0fe879da7ff65e61e338fd0aa7a80cb8409a8818
MohammadShawahni <mohammadshawahnih@gmail.com>
2021-12-29 14:36:06 +0200

    Add Link

--- F_P.js
+++ F_P.js
@@ -1,23 +1,33 @@
-var slideIndex = [1,1];
-var slideId = ["mySlides1"]
-showSlides(1, 0);
-showSlides(1, 1);
+let slideIndex =1;
+showSlides(slideIndex);
 
-function plusSlides(n, no)
+function plusSlides(n)
 {
-  showSlides(slideIndex[no] += n, no);
+  showSlides(slideIndex += n);
 }
 
-function showSlides(n, no)
+function currentSlide(n)
 {
-  var i;
-  var x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++)
-  {
-     x[i].style.display = "none";  
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n)
+{
+  let i;
+  let slides = document.getElementsByClassName("mySlides1");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
   }
-  
-  x[slideIndex[no]-1].style.display = "block";
+  slides[slideIndex-1].style.display = "block";  
+}
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
